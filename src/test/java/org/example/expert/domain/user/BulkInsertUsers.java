a6910850fa858393df74d251d2ecc6d35ddf309f
sohyun kim <sohyun050282@gmail.com>
2026-04-03 17:38:26 +0900

    feat: 속도 문제로 bulkInsert 코드 수정

--- src/test/java/org/example/expert/domain/user/BulkInsertUsers.java
+++ src/test/java/org/example/expert/domain/user/BulkInsertUsers.java
@@ -3,11 +3,12 @@ package org.example.expert.domain.user;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.UUID;
 
 @SpringBootTest
@@ -20,6 +21,41 @@ public class BulkInsertUsers {
     private PasswordEncoder passwordEncoder;
 
     @Test
+    void bulkInsertUsersTest() {
+        String sql = "INSERT INTO users (email, password, user_role, nickname, created_at, modified_at) " +
+                "VALUES (?, ?, ?, ?, NOW(), NOW())";
+        String encodedPassword = passwordEncoder.encode("password123");
+
+        int totalCount = 5000000; // 500만 건
+        int batchSize = 10000;    // 한 번에 1만 건씩 묶어서 쏘기! (DB 메모리 보호)
+
+        for (int j = 0; j < totalCount / batchSize; j++) {
+            final int batchIndex = j;
+
+            jdbcTemplate.batchUpdate(sql, new BatchPreparedStatementSetter() {
+
+                @Override
+                public void setValues(PreparedStatement ps, int i) throws SQLException {
+                    int currentId = (batchIndex * batchSize) + i;
+                    String nickname = "user_" + UUID.randomUUID().toString().substring(0, 8);
+
+                    ps.setString(1, "test" + currentId + "@test.com");
+                    ps.setString(2, encodedPassword);
+                    ps.setString(3, "USER");
+                    ps.setString(4, nickname);
+                }
+
+                @Override
+                public int getBatchSize() {
+                    return batchSize;
+                }
+            });
+
+            System.out.println((j + 1) * batchSize + "건 완료...");
+        }
+    }
+
+    /*@Test
     void bulkInsertUsers() {
         int totalCount = 5_000_000;
         int batchSize = 10_000; // 1만 건씩 끊어서 처리
@@ -45,5 +81,5 @@ public class BulkInsertUsers {
             jdbcTemplate.batchUpdate(sql, batchArgs);
             System.out.println((i + 1) * batchSize + "건 완료...");
         }
-    }
+    }*/
 }
